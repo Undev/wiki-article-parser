bf520be84a8a453f6065173437768031c1a2b6b7
Denis Denisov <denisovdenis@me.com>
2014-02-18 15:59:47 +0300

    Update ArticleParser.php

--- ArticleParser.php
+++ ArticleParser.php
@@ -1,16 +1,9 @@
 <?php
-/**
- * Author: Denisov Denis
- * Email: denisovdenis@me.com
- * Date: 28.05.13
- * Time: 17:25
- */
 
 $wgExtensionFunctions[] = 'wfSetupArticleParser';
 $wgExtensionCredits['other'][] = array(
     'path' => __FILE__,
     'name' => 'ArticleParser',
-    'author' => '[http://www.facebook.com/denisovdenis Denisov Denis]',
     'url' => 'https://github.com/Undev/wiki-article-parser',
     'description' => 'Parse article for br tag',
     'version' => 0.3,
